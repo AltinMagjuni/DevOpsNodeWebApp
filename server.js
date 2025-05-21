e6af711ee8aad879ad611139b0d29c7270d4da3e
AltinMagjuni <80954699+AltinMagjuni@users.noreply.github.com>
2025-05-21 13:47:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Deployed via Jenkins Polling);
+  res.send('Hello FS2025 DevOps Course! Deployed via Jenkins Polling');
 });
 
 app.listen(PORT, HOST);
